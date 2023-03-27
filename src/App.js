a6d64d4ca509f3ef4855dec42c7d5bc6d6449b32
Jaron <jaronchretien@yahoo.com>
2023-03-27 13:25:12 -0500

    feat(logo): Sets up spotify logo rendering

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import LoginRegister from './LoginRegister'
+import LoginRegister from './components/LoginRegister'
 import { Routes, Route } from 'react-router-dom'
 const App = () => {
     const [toke, setToken] = useState(window.localStorage.getItem('token') || null)
---
+++ src/components/LoginRegister.js
@@ -0,0 +1,18 @@
+
+
+const LoginRegister = () => {
+    return (
+        <div className="loginRegisterPage">
+            <div className="loginRegisterHeader">
+                <img className="spotifyLogo" src="/images/Spotify_logo_with_text.svg.png"/>
+            </div>
+            
+            <div className="bottomContainer">   
+            
+            </div>
+
+        </div>
+    )
+}
+
+export default LoginRegister
