09ff8244433a626ec04eafab3dbff35b3bd59117
Jaron <jaronchretien@yahoo.com>
2023-03-29 18:09:43 -0500

    feat(AccessPage): Adds grant access page and button" : ;

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import LoginRegister from './components/LoginRegister'
+import Access from './components/Access'
 import { Routes, Route } from 'react-router-dom'
 const App = () => {
-    const [toke, setToken] = useState(window.localStorage.getItem('token') || null)
+    const [token, setToken] = useState(window.localStorage.getItem('token') || null)
     return (
         <Routes>
         
-        <Route path='account' element={<LoginRegister />} />
-  
+        <Route path='/access' element={<Access />}/>
         
         </Routes>
     )
--- src/components/Access.js
+++ src/components/Access.js
@@ -3,7 +3,14 @@
 const Access = () => {
     return (
         <div className="accesPage">
-
+            <div className="loginRegisterHeader">
+                <img className="spotifyLogo" src="/images/Spotify_logo_with_text.svg.png"/>
+            </div>
+            <div className="centerContainer">
+                <h2>It appears you do not have access!</h2>
+                <button>Grant Access</button>
+            </div>
         </div>
     )
 }
+export default Access
