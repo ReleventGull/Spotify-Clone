e86bf6a03dfa1f9bc0a0d26c886c1ebd532d8178
Jaron <jaronchretien@yahoo.com>
2023-04-05 15:46:59 -0500

    feat(images): Adds images

--- src/Application.js
+++ src/Application.js
@@ -1,7 +1,23 @@
 import { Outlet } from "react-router-dom"
+import {Link} from 'react-router-dom'
 const Application = () => {
     return (
+        <>
+        <div className="navBar">
+            <div className="container1Nav">
+                <div className="topBar">
+                    <Link><img className='logo' src='/images/spotifyProfile.png' />Profile</Link>
+                    <Link><img className='logo' src='/images/spotifyHome.png' />  Home</Link>
+                    <Link><img className='logo' src='/images/spotifySearch.png' /> Search</Link>
+                    <Link><img className='logo' src='/images/spotifyLibrary.png' /> Your Library</Link>
+                </div>
+            </div>
+            
+        </div>
         < Outlet/>
+        </>
+        
+
     )
 }
 export default Application
